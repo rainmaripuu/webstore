a135f492b3f982897e22afb829f09c16ea97e472
Rain <rain.maripuu@gmail.com>
2020-08-09 10:01:08 +0300

    Sorting (order_by) and Searching (filter)
    sorting and searching implemented in shop.html
    due to button configuration, sorting is not very elegant, three for_loops running simultaneously, displaying active card (newest, price, -price)

--- webstore/store/views.py
+++ webstore/store/views.py
@@ -11,13 +11,30 @@ def home_view(request):
     return render(request, 'index.html', context)
 
 
+def filter_context(filter_val, shop_context):
+    shop_context = Product.objects.filter(name__icontains=filter_val) | \
+        shop_context.filter(category__name__icontains=filter_val)
+    return shop_context
+
+
 def shop_view(request):
     item = Product.objects.all()
+    filter_val = request.GET.get('filter', None)
     newest = Product.objects.order_by('-added_date')
+    if filter_val:
+        newest = Product.objects.filter(name__icontains=filter_val) | \
+            newest.filter(category__name__icontains=filter_val)
     price_high_low = Product.objects.order_by('-price')
+    if filter_val:
+        price_high_low = Product.objects.filter(name__icontains=filter_val) | \
+            price_high_low.filter(category__name__icontains=filter_val)
     price_low_high = Product.objects.order_by('price')
+    if filter_val:
+        price_low_high = Product.objects.filter(name__icontains=filter_val) | \
+            price_low_high.filter(category__name__icontains=filter_val)
     context = {'products': item, 'newest': newest, 'price_high_low': price_high_low, 'price_low_high': price_low_high}
-
+    context['orderby'] = request.GET.get('orderby', 'newest')
+    context['filter'] = request.GET.get('filter', None)
     return render(request, 'shop.html', context)
 
 
