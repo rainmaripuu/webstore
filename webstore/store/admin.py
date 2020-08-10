c2ef49c5df6e0248ba2611e467978430a086fe3a
Rain <rain.maripuu@gmail.com>
2020-08-10 12:53:12 +0300

    Category Tree
    Category tree added using django-mptt

--- webstore/store/admin.py
+++ webstore/store/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from store.models import Product, Category, StoreUser, Cart
+from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 
 
 # Register your models here.
 
 admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Category, DraggableMPTTAdmin)
 admin.site.register(StoreUser)
 admin.site.register(Cart)
--- webstore/store/models.py
+++ webstore/store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from store.constants import COUNTRIES
+from mptt.models import MPTTModel, TreeForeignKey
 import os
 
 # Create your models here.
@@ -10,11 +11,39 @@ def get_image_path(instance, filename):
     return os.path.join('products', str(instance.id), filename)
 
 
-class Category(models.Model):
-    class Meta:
-        verbose_name_plural = "categories"
+# class Category(models.Model):
+#     class Meta:
+#         verbose_name_plural = "categories"
+#
+#     name = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         return self.name
 
-    name = models.CharField(max_length=20)
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True, on_delete=models.DO_NOTHING)
+    slug = models.SlugField()
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        unique_together = (('parent', 'slug',))
+        verbose_name_plural = 'categories'
+
+    def get_slug_list(self):
+        try:
+            ancestors = self.get_ancestors(include_self=True)
+        except:
+            ancestors = []
+        else:
+            ancestors = [i.slug for i in ancestors]
+        slugs = []
+        for i in range(len(ancestors)):
+            slugs.append('/'.join(ancestors[:i+1]))
+        return slugs
 
     def __str__(self):
         return self.name
@@ -27,8 +56,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     info = models.TextField(null=True, blank=True)
     added_date = models.DateField(auto_now_add=True)
-    category = models.ManyToManyField(Category)
+    category = TreeForeignKey('Category',null=True,blank=True, on_delete=models.DO_NOTHING)
     img = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.name
